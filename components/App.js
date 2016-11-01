554e0cdbc677fed2c792bb53fe8ba6fbc2af52f0
Myles O'Connor <myles@gumgum.com>
2016-11-01 14:40:06 -0700

    Added example stylesheet

--- components/App.js
+++ components/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+    toggle = () => {
+        console.log('Toggle clicked')
+    }
+
     render() {
         return (
-            <h1>Hello, World!</h1>
+            <html>
+                <head>
+                    <title>Server-side rendering with React</title>
+                    <link rel='stylesheet' href='./style.css' />
+                </head>
+                <body>
+                    <h1>Hello, World!</h1>
+                    <p>Sample paragraph</p>
+                    <button onClick={this.toggle}>Toggle</button>
+                </body>
+            </html>
         )
     }
 }
